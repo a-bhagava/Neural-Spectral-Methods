curledwake PDE initial implementation completed, signs on the equation/spectral methods fixed

src/pde/curledwake/__init__.py
@@ -82,11 +82,12 @@ def velocity_field(u=None,
                    D=1,
                    N_vortex=32,
                    sigma_vortex=0.1,
-                   dx=0.2, 
-                   dy=0.1,
-                   dz=0.1,
                    turbine_y=0.0, 
                    turbine_z=0.0,):
+    
+    y_coords = np.linspace(-2, 2, u.shape[1])
+    z_coords = np.linspace(-2, 2, u.shape[2])
+    dz = z_coords[1] - z_coords[0]
 
     # clip edges to prevent singularities
     r_i = np.linspace(-(D - dz) / 2, (D - dz) / 2, N_vortex)
@@ -99,8 +100,6 @@ def velocity_field(u=None,
     sigma = sigma_vortex * D
 
     # main summation, which is 3D (y, z, i)
-    y_coords = np.linspace(-2, 2, u.shape[1])
-    z_coords = np.linspace(-2, 2, u.shape[2])
     yG, zG = np.meshgrid(y_coords, z_coords, indexing="ij")
     yG = yG[..., None]
     zG = zG[..., None]
@@ -115,39 +114,59 @@ def velocity_field(u=None,
 
     # tile along the x direction 
     v = np.broadcast_to(v_slice, (u.shape[0], *v_slice.shape))
-    w = np.broadcast_to(w_slice, (u.shape[0], *w_slice.shape))
+    w = np.broadcast_to(w_slice, (u.shape[0], *w_slice.shape))    
     return v, w
 
 
 class CurledWake(PDE):
     def __str__(self):
-        return f"CurledWake: {self.T}T, {self.nu}ν, {self.σ}σ"
+        return f"CurledWake: X={self.X}, ν={self.nu}"
     
-    def __init__(self): 
-        self.nu = None
-        pass 
+    def __init__(self, ic: WakeInitial, X: float, nu: float): 
+
+        self.odim = 1
+        self.ic = ic
+
+        self.X = X
+        self.nu = nu
+        self.fn = None  # no forcing term for now
+
+        self.l = (l:=ic.length)
+        self.Re = l / nu * ic.scaling
+
+        self.domain = Rect(3)
+        self.basis = series(Chebyshev, Fourier, Fourier)
+        self.params = Interpolate(ic, self.basis)
+
+        from ..mollifier import initial_condition
+        self.mollifier = initial_condition
 
     @F.cached_property
     def solution(self):
-        pass
+
+        dir = os.path.dirname(__file__)
+
+        with jax.default_device(jax.devices("cpu")[0]):
+
+            _u = np.load(f"{dir}/_u.{self.ic}.npy")
+            _u_full = np.load(f"{dir}/u.{self}.npy") # TODO: check this makes sense after data generation scripts are written
+            
+        return jax.vmap(self.basis)(_u), _u_full.shape[1:-1], _u_full
 
     def equation(self, x: X, _u0: X, _u: X): 
-
         _u, _u1, _u2 = utils.fdm(_u, n=2)
         _ux = _u1[..., 0, 0]
         _uy = _u1[..., 0, 1]
         _uz = _u1[..., 0, 2]
         _Δu = Δ(_u2[..., 0, 1:, 1:])
 
-        v, w = self.v, self.w
-        nu_T = self.nu # fix to -- 10^-3 -- can this be learnt instead ? how does projection layer work? 
-        _Dudx = _ux / self.X + v * _uy + w * _uz # self.X is the scaling factor 
+        v, w = velocity_field(u=_u.squeeze(-1))
+        _Dudx = _ux / self.X + (v * _uy + w * _uz) # self.X is the scaling factor TODO: verify with kirby, division by U
 
-        # forcing term -- is this the TKE? 
         if self.fn is None: f = np.zeros_like(_Dudx)
-        else: f = self.fn(*_u[0].squeeze(-1).shape) # unsure if this is correct
+        else: f = self.fn(*_u[0].squeeze(-1).shape)
 
-        return (_Dudx - nu_T * _Δu - f) / self.u 
+        return (_Dudx + self.nu * _Δu + f) # TODO: check with kirby about the -1/U division and the addition/subtraction signs here
 
     def boundary(self, _u: List[Tuple[X]])-> List[X]: 
         _, (_ut, _ub), (_ul, _ur) = _u
@@ -162,13 +181,14 @@ class CurledWake(PDE):
         _uz = self.basis(_u1.coef[..., 0, 2])
         _Δu = self.basis(Δ(_u2.coef[..., 0, 1:, 1:]))
 
-        v, w = jax.vmap(velocity_field)() 
-        nu_T = self.nu  # unsure if i need to compute this just once or every time
+        v, w = velocity_field(u=_u.inv().squeeze(-1))
         _Dudx = self.basis.add(
             _ux.map(lambda coef: coef / self.X),
             self.basis.transform(v * _uy.inv() + w * _uz.inv())
         )
+        
         # forcing term
         if self.fn is None: f = self.basis(np.zeros_like(_Dudx.coef))
         else: f = self.basis.transform(np.broadcast_to(self.fn(*_u.mode[1:]), _u.mode))
-        return self.basis.add(_Dudx, self.basis(-nu_T * _Δu.coef), f.map(np.negative)) / self.u
+
+        return self.basis.add(_Dudx, self.basis(self.nu * _Δu.coef), f.map(np.negative)) # TODO: check with kirby about the -1/U division and the addition/subtraction signs here